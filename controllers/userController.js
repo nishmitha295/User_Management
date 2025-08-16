2836df83fcd05bcdd31ddb0c62f08ddcc405de49
nishmitha295 <mvnishmithagowda@gmail.com>
2025-08-16 22:44:38 +0530

    Add GET APIs to fetch all users and fetch user by ID

--- controllers/userController.js
+++ controllers/userController.js
@@ -24,4 +24,41 @@ exports.createUser = async (req, res, next) => {
   }
 };
 
+// Get All Users
+exports.getUsers = async (req, res, next) => {
+  try {
+    const users = await userService.getAllUsers();
+    res.json({
+      success: true,
+      message: "Users retrieved successfully",
+      data: users,
+      count: users.length,
+    });
+  } catch (error) {
+    next(error);
+  }
+};
+
+// Get User by ID
+exports.getUserById = async (req, res, next) => {
+  try {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      return res.status(400).json({
+        success: false,
+        message: "Validation failed",
+        errors: errors.array(),
+      });
+    }
+
+    const user = await userService.getUserById(req.params.id);
+    res.json({
+      success: true,
+      message: "User retrieved successfully",
+      data: user,
+    });
+  } catch (error) {
+    next(error);
+  }
+};
 
